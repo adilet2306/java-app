539719d43e124185c2da6d1ab8cc947ad126e945
adilet2306 <adilet.abdraimov9@gmail.com>
2024-04-22 05:24:24 +0000

    new changes

---
+++ HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class HelloWorldTest {
+    @Test
+    public void testMain() {
+        assertTrue("The test should always pass", true);
+    }
+}
